b43288b7ad43577dd6a98eb8fda21bdc69866bba
Oli <hewisonoliver@gmail.com>
2022-01-29 22:22:34 +0000

    Player Animations (front only rn coffee)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,24 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     public static PlayerMovement INSTANCE;
+    private Animator[] animators;
 
     // Start is called before the first frame update
     void Start()
     {
         INSTANCE = this;
         rb = GetComponent<Rigidbody2D>();
+        animators = GetComponentsInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") ).normalized * moveSpeed;
+        foreach (Animator animator in animators)
+        {
+            animator.SetFloat("Velocity", dir.magnitude);
+        }
         rb.velocity = dir;
     }
 }
